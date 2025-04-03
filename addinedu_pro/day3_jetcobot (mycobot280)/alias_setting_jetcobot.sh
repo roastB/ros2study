c90d0e408e1e23d1f92291e48670788995d87e06
roastB <iceative12@gmail.com>
2025-04-03 22:48:47 +0900

    Add alias setting for jetcobot(alias_setting_jetcobot.sh)

---
+++ addinedu_pro/day3_jetcobot (mycobot280)/alias_setting_jetcobot.sh
@@ -0,0 +1,10 @@
+# ALIAS SETTINGS [Basic]
+alias sb="source ~/.bashrc; echo \"Bashrc is reloaded!\"" # source bashrc 줄여서 sb
+alias ros_domain="export ROS_DOMAIN_ID=99; echo \"ROS_DOMAIN_ID is set to 99!\""
+alias jazzy="source /opt/ros/jazzy/setup.bash; ros_domain; echo \"ROS2 Jazzy is activated!\""
+
+# ALIAS SETTINGS [Main]
+alias ssh_jetcobot="ssh jetcobot@192.168.5.1"
+alias ssh_pinky="ssh pinky@192.168.4.1"
+alias sv="source /venv/seobo_venv/bin/activate"
+alias jn="jupyter notebook"
